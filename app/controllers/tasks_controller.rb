429d5cf7296f646d181e0650290d22c273f50658
yonathan-palma <yonathanpalmarey@gmail.com>
2024-02-22 16:44:33 -0400

    rails tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,43 @@ class TasksController < ApplicationController
     @tasks = Task.all
     # raise
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params_task)
+    # raise
+    if @task.save
+      redirect_to tasks_path
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params_task)
+    redirect_to tasks_path(@tasks)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def params_task
+    params.require(:task).permit(:title, :details)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,16 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get 'tasks', to: 'tasks#index', as: :index
-  # get "tasks/:id/edit", to: "tasks#edit"
-  # patch "tasks/:id", to: "tasks#update"
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/edit/:id', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
 
-  # get "tasks/new", to: "tasks#new"
-  # post "tasks", to: "tasks#create"
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
 
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 
-  # get "tasks/:id", to: "tasks#show", as: "task"
-
-  # delete "tasks/:id", to: "tasks#destroy"
-
+  delete 'tasks/:id', to: 'tasks#destroy'
 
   # Defines the root path route ("/")
   # root "posts#index"
